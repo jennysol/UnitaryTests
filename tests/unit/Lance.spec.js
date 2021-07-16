61b1c7115a007aef7935fadeaead65d2b04d3d61
jennysol <jennyssoliver@gmail.com>
2021-07-16 20:58:41 -0300

    Feature: Find element inout

--- tests/unit/Lance.spec.js
+++ tests/unit/Lance.spec.js
@@ -2,6 +2,7 @@ import Lance from '@/components/Lance'
 import { mount } from '@vue/test-utils'
 
 test('Does not accept negative bids', () => {
-    const wrapper = mount(Lance)
-    expect(wrapper).toBeTruthy()
+  const wrapper = mount(Lance)
+  const input = wrapper.find('input')
+  expect(input).toBeTruthy()
 })
