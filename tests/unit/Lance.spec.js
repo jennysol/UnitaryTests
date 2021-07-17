d7a1faf486088dc0e7681597f88405083c2aea57
jennysol <jennyssoliver@gmail.com>
2021-07-16 21:55:33 -0300

    Feature: Add Positiv test

--- tests/unit/Lance.spec.js
+++ tests/unit/Lance.spec.js
@@ -17,3 +17,14 @@ test('Does not accept negative bids', () => {
   wrapper.trigger('submit')
   expect(lancesEmitidos).toBeUndefined()
 })
+
+test('Place a bid when the value is greater than zero', () => {
+  const wrapper = mount(Lance)
+
+  const input = wrapper.find('input')
+
+  input.setValue(100)
+  wrapper.trigger('submit')
+  const lancesEmitidos = wrapper.emitted('novo-lance')
+  expect(lancesEmitidos).toHaveLength(1)
+})
