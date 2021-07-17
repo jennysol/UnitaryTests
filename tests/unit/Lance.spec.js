64c1edb5d88d6e059a59c2d34483bd126531e159
jennysol <jennyssoliver@gmail.com>
2021-07-16 21:23:32 -0300

    Feature: Add does not accept test

--- tests/unit/Lance.spec.js
+++ tests/unit/Lance.spec.js
@@ -3,6 +3,17 @@ import { mount } from '@vue/test-utils'
 
 test('Does not accept negative bids', () => {
   const wrapper = mount(Lance)
+
+  // Finding our input
   const input = wrapper.find('input')
-  expect(input).toBeTruthy()
+
+  // Sending value
+  input.setValue(-100)
+
+  // Listening to Event issued
+  const lancesEmitidos = wrapper.emitted('novo-lance')
+
+  // Activating Submit
+  wrapper.trigger('submit')
+  expect(lancesEmitidos).toBeUndefined()
 })
