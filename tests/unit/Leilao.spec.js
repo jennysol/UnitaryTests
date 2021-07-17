e40a474bd35f84d5e3660dafea3a77db2ba78f41
jennysol <jennyssoliver@gmail.com>
2021-07-17 14:59:15 -0300

    Feature: Displays auction data on card

--- tests/unit/Leilao.spec.js
+++ tests/unit/Leilao.spec.js
@@ -1,19 +1,27 @@
-import Leilao from '@/components/Lance'
+
+import Leilao from '@/components/Leilao'
 import { mount } from '@vue/test-utils'
 
 const leilao = {
-  produto: 'Book',
-  lanceInicial: 49,
-  descricao: 'Um maravilhoso livro'
+  produto: 'Livro da Casa do Código',
+  lanceInicial: 50,
+  descricao: 'Livro sobre VueJS'
 }
 
-describe('Um leilão exibe os dados do produto', () => {
-  test('Exibe os dados do leilão no card', () => {
+describe('An auction is displayed correctly', () => {
+  test('Displays auction data on card', () => {
     const wrapper = mount(Leilao, {
       propsData: {
         leilao
       }
     })
-    expect(wrapper).toBeTruthy()
+
+    const header = wrapper.find('.card-header').element
+    const title = wrapper.find('.card-title').element
+    const text = wrapper.find('.card-text').element
+
+    expect(header.textContent).toContain(`Estamos leiloando um(a): ${leilao.produto}`)
+    expect(title.textContent).toContain(`Lance inicial: R$ ${leilao.lanceInicial}`)
+    expect(text.textContent).toContain(leilao.descricao)
   })
 })
