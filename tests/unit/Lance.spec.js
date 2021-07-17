85ba94270774d87c0847fbb33d3cea232aba7bd9
jennysol <jennyssoliver@gmail.com>
2021-07-16 22:25:22 -0300

    Feature: All bids must have an amount greater than the minimum entered.

--- tests/unit/Lance.spec.js
+++ tests/unit/Lance.spec.js
@@ -41,3 +41,19 @@ describe('A bid with no minimum value', () => {
     expect(lance).toBe(100)
   })
 })
+
+describe('A bid with minimum value', () => {
+  test('All bids must have an amount greater than the minimum entered.', () => {
+    const wrapper = mount(Lance, {
+      // Valor minímo é passado via prop
+      propsData: {
+        lanceMinimo: 300
+      }
+    })
+    const input = wrapper.find('input')
+    input.setValue(400)
+    wrapper.trigger('submit')
+    const lancesEmitidos = wrapper.emitted('novo-lance')
+    expect(lancesEmitidos).toHaveLength(1)
+  })
+})
