2a60efceecacb486ed123aafd91e7903072d37a6
jennysol <jennyssoliver@gmail.com>
2021-07-20 07:48:23 -0300

    Feature:Ensuring the operation the business rule

--- tests/unit/Leiloeiro.spec.js
+++ tests/unit/Leiloeiro.spec.js
@@ -12,6 +12,27 @@ const leilao = {
   descricao: 'Livro bem bacana'
 }
 
+const lances = [
+  {
+    id: 1,
+    valor: 1001,
+    data: '2020-06-13T18:04:26.826Z',
+    leilao_id: 1
+  },
+  {
+    id: 2,
+    valor: 1002,
+    data: '2020-06-13T18:04:26.826Z',
+    leilao_id: 1
+  },
+  {
+    id: 3,
+    valor: 1003,
+    data: '2020-06-13T18:04:26.826Z',
+    leilao_id: 1
+  }
+]
+
 describe('Auctioneer starts an auction that has no bids', () => {
   test('Notify me when there are no bids', async () => {
     getLeilao.mockResolvedValueOnce(leilao)
@@ -28,5 +49,63 @@ describe('Auctioneer starts an auction that has no bids', () => {
     const alerta = wrapper.find('.alert-dark')
 
     expect(alerta.exists()).toBe(true)
+  })
+})
+
+describe('An auctioneer displays existing bids', () => {
+  test('Does not show "No bids" warning', async () => {
+    getLeilao.mockResolvedValueOnce(leilao)
+    getLances.mockResolvedValueOnce(lances)
+
+    const wrapper = mount(Leiloeiro, {
+      propsData: {
+        id: 1
+      }
+    })
+    await flushPromises()
+    const alerta = wrapper.find('.alert-dark')
+    expect(alerta.exists()).toBe(false)
+  })
+  test('Auctioneer has a list of bids', async () => {
+    getLeilao.mockResolvedValueOnce(leilao)
+    getLances.mockResolvedValueOnce(lances)
+
+    const wrapper = mount(Leiloeiro, {
+      propsData: {
+        id: 1
+      }
+    })
+    await flushPromises()
+    const alerta = wrapper.find('.list-inline')
+    expect(alerta.exists()).toBe(true)
+  })
+})
+
+describe('An auctioneer communicates the highest and lowest bid amounts', () => {
+  test('Shows the highest bid for that auction', async () => {
+    getLeilao.mockResolvedValueOnce(leilao)
+    getLances.mockResolvedValueOnce(lances)
+
+    const wrapper = mount(Leiloeiro, {
+      propsData: {
+        id: 1
+      }
+    })
+    await flushPromises()
+    const maiorLance = wrapper.find('.maior-lance')
+    expect(maiorLance.element.textContent).toContain('Maior lance: R$ 1003')
+  })
+  test('Shows the lowest bid for that auction', async () => {
+    getLeilao.mockResolvedValueOnce(leilao)
+    getLances.mockResolvedValueOnce(lances)
+
+    const wrapper = mount(Leiloeiro, {
+      propsData: {
+        id: 1
+      }
+    })
+    await flushPromises()
+    const menorLance = wrapper.find('.menor-lance')
+    expect(menorLance.element.textContent).toContain('Menor lance: R$ 1001')
   })
 })
